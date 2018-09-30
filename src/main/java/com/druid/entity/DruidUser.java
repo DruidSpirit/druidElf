9bcc93a09d0712ca199c82d07f1485d9a7115f73
druid <844748180@qq.com>
2018-09-30 11:32:35 +0800

    新增lombok插件，index页面微调整

--- src/main/java/com/druid/entity/DruidUser.java
+++ src/main/java/com/druid/entity/DruidUser.java
@@ -1,7 +1,11 @@
 package com.druid.entity;
 
-import javax.persistence.*;
+import lombok.Builder;
+import lombok.Data;
 
+import javax.persistence.*;
+@Data
+@Builder
 @Table(name = "druid_user")
 public class DruidUser {
     /**
@@ -20,57 +24,4 @@ public class DruidUser {
      */
     private String password;
 
-    /**
-     * 获取id
-     *
-     * @return id - id
-     */
-    public Integer getId() {
-        return id;
-    }
-
-    /**
-     * 设置id
-     *
-     * @param id id
-     */
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    /**
-     * 获取用户名
-     *
-     * @return username - 用户名
-     */
-    public String getUsername() {
-        return username;
-    }
-
-    /**
-     * 设置用户名
-     *
-     * @param username 用户名
-     */
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    /**
-     * 获取密码
-     *
-     * @return password - 密码
-     */
-    public String getPassword() {
-        return password;
-    }
-
-    /**
-     * 设置密码
-     *
-     * @param password 密码
-     */
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
